da9ae299123cfd72099a3db15a5d459bb9b0dde3
U-SVA\phermann <phermann@TPPHERMANN.sva.de>
2019-03-13 17:47:22 +0100

    added a lot more

--- 20190318-pds-aufgabe-1p-hermann.py
+++ 20190318-pds-aufgabe-1p-hermann.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-#die umrechnungsfaktoren beziehen sich immer in die umrechung in die zielmaßeinheit meter
-umrechnungsfaktoren_metrisch = {
+#conversion_factors beziehen sich immer in die umrechung in die zielmaßeinheit meter
+conversion_factors_metric = {
     "mum": 1000000,
     "mm": 1000,
     "cm": 100,
@@ -10,54 +10,71 @@ umrechnungsfaktoren_metrisch = {
     "km" : 1/1000,
 }
 
-umrechnungsfaktoren_andere = {
+conversion_factors_english_units = {
     "in": 1,
     "ft": 1/12,
     "yd": 1/36,
-    "mi": 1/63360, #->stimmt nicht!
+    "mi": 1/63360,
 }
 
-umrechnungsfaktor_meter_inch = 39.37
+conversion_factors_meter_inch = 39.37
 
 def conv_length (value_in, unit_in, unit_out):
 
-    #hier die execption zum werte pruefen!!
+    #exception to make sure value_in contains a int or float number
+    try:
+        float(value_in)
+
+        if value_in <= 0:
+            raise ValueError
+
+    except ValueError:
+        print (value_in, "is not a (positive) number.., program exited")
+        exit()
 
     # hier wird ueberprueft ob die eingangswerte existieren oder nicht
+    if unit_in not in conversion_factors_metric.keys() and unit_in not in conversion_factors_english_units.keys():
+        print ("unit_in is not available")
 
-    if unit_in in umrechnungsfaktoren_metrisch.keys() and unit_out in umrechnungsfaktoren_metrisch.keys():
-        print("ok im metrischen bereich!")
-        ergebnis = value_in * (umrechnungsfaktoren_metrisch[unit_out] / umrechnungsfaktoren_metrisch[unit_in])
+        if unit_out not in conversion_factors_metric.keys() and unit_out not in conversion_factors_english_units.keys():
+            print ("unit_out is not available")
 
-    if unit_in in umrechnungsfaktoren_andere.keys() and unit_out in umrechnungsfaktoren_andere.keys():
-        print("ok im NICHT-metrischen bereich!")
-        ergebnis = value_in * (umrechnungsfaktoren_andere[unit_out] / umrechnungsfaktoren_andere[unit_in])
+        print("exiting...")
+        exit()
+
+    # hier wird ueberprueft ob die eingangswerte existieren oder nicht
+    if unit_out not in conversion_factors_metric.keys() and unit_out not in conversion_factors_english_units.keys():
+        print ("unit_out not available, program exited")
+        exit()
+
+    #wenn der ein- und ausgabewert metrisch ist
+    if unit_in in conversion_factors_metric.keys() and unit_out in conversion_factors_metric.keys():
+        result = value_in * (conversion_factors_metric[unit_out] / conversion_factors_metric[unit_in])
+
+    #wenn der ein- und ausgabewert nicht metrisch ist
+    if unit_in in conversion_factors_english_units.keys() and unit_out in conversion_factors_english_units.keys():
+        result = value_in * (conversion_factors_english_units[unit_out] / conversion_factors_english_units[unit_in])
 
     #wenn der eingabewert metrisch ist und in ein 'anderes' format umgewandelt werden soll
-    if unit_in in umrechnungsfaktoren_metrisch.keys() and unit_out in umrechnungsfaktoren_andere.keys():
-        ergebnis_metrisch = value_in * (umrechnungsfaktoren_metrisch["m"] / umrechnungsfaktoren_metrisch[unit_in])
-        ergebnis_in_zoll = ergebnis_metrisch * umrechnungsfaktor_meter_inch
-        ergebnis = ergebnis_in_zoll * (umrechnungsfaktoren_andere[unit_out] / umrechnungsfaktoren_andere["in"])
+    if unit_in in conversion_factors_metric.keys() and unit_out in conversion_factors_english_units.keys():
+        result_metric = value_in * (conversion_factors_metric["m"] / conversion_factors_metric[unit_in])
+        result_in_inch = result_metric * conversion_factors_meter_inch
+        result = result_in_inch * (conversion_factors_english_units[unit_out] / conversion_factors_english_units["in"])
 
     #wenn der eingabewert nicht metrisch ist und in ein metrisches format umgewandelt werden soll
-    if unit_in in umrechnungsfaktoren_andere.keys() and unit_out in umrechnungsfaktoren_metrisch.keys():
-        ergebnis_anders = value_in * (umrechnungsfaktoren_andere["in"] / umrechnungsfaktoren_andere[unit_in])
-        ergebnis_in_meter = ergebnis_anders / umrechnungsfaktor_meter_inch
-        ergebnis = ergebnis_in_meter * (umrechnungsfaktoren_metrisch[unit_out] / umrechnungsfaktoren_metrisch["m"])
+    if unit_in in conversion_factors_english_units.keys() and unit_out in conversion_factors_metric.keys():
+        result_anders = value_in * (conversion_factors_english_units["in"] / conversion_factors_english_units[unit_in])
+        result_in_meter = result_anders / conversion_factors_meter_inch
+        result = result_in_meter * (conversion_factors_metric[unit_out] / conversion_factors_metric["m"])
 
 
-
-    print("Eingangswert:", value_in, unit_in )
-    print("---> Zielmaßeinheit:", unit_out)
-    print("\n",ergebnis, unit_out)
-
-
+    print("converted:", value_in,unit_in,"to", result,unit_out)
 
 
 def main():
-    value_in = 500
-    unit_in = "mm"
-    unit_out = "ft"
+    value_in = 5
+    unit_in = "in"
+    unit_out = "mi"
     conv_length(value_in, unit_in, unit_out)
 
 
